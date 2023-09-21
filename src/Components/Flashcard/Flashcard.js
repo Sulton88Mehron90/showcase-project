8508fddd14c1999d29138a6a37c57bf84d19252b
Parvin Sattorova <nanajon66@gmail.com>
2023-09-20 23:24:54 -0600

    refactor:Add form css file

--- src/Components/Flashcard/Flashcard.js
+++ src/Components/Flashcard/Flashcard.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-// import React, { useState, useEffect, useRef } from 'react'
+// import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types';
 import './Flashcard.css';
 
@@ -7,20 +7,20 @@ export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false);
   const [height, setHeight] = useState('initial')
 
-  // const frontEl = useRef()
-  // const backEl = useRef()
+  const frontEl = useRef()
+  const backEl = useRef()
 
-  // function setMaxHeight() {
-  //   const frontHeight = frontEl.current.getBoundingClientRect().height
-  //   const backHeight = backEl.current.getBoundingClientRect().height
-  //   setHeight(Math.max(frontHeight, backHeight, 100))
-  // }
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backHeight, 100))
+  }
 
-  // useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
-  // useEffect(() => {
-  //   window.addEventListener('resize', setMaxHeight)
-  //   return () => window.removeEventListener('resize', setMaxHeight)
-  // }, [])
+  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return () => window.removeEventListener('resize', setMaxHeight)
+  }, [])
 
 
   console.log("Flashcard object:", flashcard);  // dont forget to delete this console
@@ -29,8 +29,8 @@ export default function Flashcard({ flashcard }) {
     <div className={`card ${flip ? 'flip' : ''}`}
       onClick={() => setFlip(!flip)}
     >
-      <div className='front'>
-      {/* <div className="front" ref={frontEl}> */}
+      {/* <div className='front'> */}
+      <div className="front" ref={frontEl}>
         {flashcard.question}
         <div className='flashcard-options'>
           {flashcard.options.map((option, index) => {
@@ -39,7 +39,13 @@ export default function Flashcard({ flashcard }) {
         </div>
       </div>
       {/* <div className="back" ref={backEl}>{flashcard.correctAnswer}</div> */}
-      <div className='back'>{flashcard.correctAnswer}</div>
+      {/* <div className='back'>{flashcard.correctAnswer}</div> */}
+      <div className="back" ref={backEl}>
+        {flip && (
+          <span className="correct-answer-emoji" role="img" aria-label="Correct Answer">🌱</span>
+        )}
+        {flashcard.correctAnswer}
+      </div>
     </div>
   )
 }
