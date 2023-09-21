f84030d1b9cc3ff940ad6afdd184a187efb5d0ec
Parvin Sattorova <nanajon66@gmail.com>
2023-09-20 20:02:43 -0600

    refactor the App component to have the categoryies be fetched in useEffect and add form in flashcard container component

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,16 +1,33 @@
-
-export function getTrivia(amount = 50, type = 'multiple', encode = 'url3986') {
-    const API_ENDPOINT = `https://opentdb.com/api.php?amount=${amount}&type=${type}&encode=${encode}`;
-    
-    return fetch(API_ENDPOINT)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
+export function getTrivia(amount = 50, category, type = 'multiple', encode = 'url3986') {
+  let API_ENDPOINT = `https://opentdb.com/api.php?amount=${amount}&type=${type}&encode=${encode}`;
+  if (category) {
+    API_ENDPOINT += `&category=${category}`;
   }
   
+  return fetch(API_ENDPOINT)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+export function getCategories() {
+  const CATEGORY_API_ENDPOINT = "https://opentdb.com/api_category.php";
+
+  return fetch(CATEGORY_API_ENDPOINT)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => data.trivia_categories) 
+    .catch(error => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,17 +2,57 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import FlashcardContainer from '../FlashcardContainer/FlashcardContainer';
 import HomePage from '../HomePage/HomePage';
-import { getTrivia } from '../../ApiCalls';
-// import './App.css';
+import { getTrivia, getCategories } from '../../ApiCalls';
+import '../Flashcard/Flashcard.css'
 
-function App() {
+export default function App() {
   const [flashcards, setFlashcards] = useState([]);
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [numberOfQuestions, setNumberOfQuestions] = useState(10);
+
 
   useEffect(() => {
     setLoading(true);
-    getTrivia()
+    getTrivia(numberOfQuestions, selectedCategory)
+      .then(data => {
+        const formattedQuestions = data.results.map((rawQuestion, index) => {
+          const decodedQuestion = decodeURIComponent(rawQuestion.question);
+          const decodedCorrectAnswer = decodeURIComponent(rawQuestion.correct_answer);
+          const decodedIncorrectAnswers = rawQuestion.incorrect_answers.map(decodeURIComponent);
+  
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodedQuestion,
+            correctAnswer: decodedCorrectAnswer,
+            options: [...decodedIncorrectAnswers, decodedCorrectAnswer].sort(() => Math.random() - 0.5),
+          };
+        });
+        setFlashcards(formattedQuestions);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('There was an error fetching trivia questions:', error);
+      });
+  }, []);
+  
+
+  useEffect(() => {
+    getCategories()
+      .then(data => {
+        setCategories(data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching trivia categories:', error);
+      });
+  }, []);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    getTrivia(numberOfQuestions, selectedCategory)
       .then(data => {
         const formattedQuestions = data.results.map((rawQuestion, index) => {
           const decodedQuestion = decodeURIComponent(rawQuestion.question);
@@ -27,20 +67,61 @@ function App() {
           };
         });
         setFlashcards(formattedQuestions);
-        console.log(formattedQuestions);  //console.log dont forget to remove
         setLoading(false);
       })
       .catch(error => {
         console.error('There was an error fetching trivia questions:', error);
       });
-  }, []);
-  
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/flashcards" element={loading ? <div>Loading...</div> : <FlashcardContainer flashcards={flashcards} />} />
-    </Routes>
+  };  
+
+//   return (
+//     <div>
+//       <form className="header" onSubmit={handleSubmit}>
+//         <div className="form-group">
+//           <label htmlFor="category">Category</label>
+//           <select id="category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+//             {categories.map(category => {
+//               return <option value={category.id} key={category.id}>{category.name}</option>
+//             })}
+//           </select>
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="amount">Number of Questions</label>
+//           <input type="number" id="amount" min="1" step="1" defaultValue={10} onChange={(e) => setNumberOfQuestions(e.target.value)} />
+//         </div>
+//         <div className="form-group">
+//           <button className="btn">Generate</button>
+//         </div>
+//       </form>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/flashcards" element={loading ? <div>Loading...</div> : <FlashcardContainer flashcards={flashcards} />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route 
+          path="/flashcards" 
+          element={
+            <FlashcardContainer 
+              flashcards={flashcards} 
+              loading={loading}
+              categories={categories}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+              numberOfQuestions={numberOfQuestions}
+              setNumberOfQuestions={setNumberOfQuestions}
+              handleSubmit={handleSubmit}
+            /> 
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
-export default App;
