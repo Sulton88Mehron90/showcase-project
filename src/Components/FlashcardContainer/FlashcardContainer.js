7a7c5a9b8c6f5546e08b8f829f68f3910bfffba9
Parvin Sattorova <nanajon66@gmail.com>
2023-09-22 08:29:47 -0600

    feature: Add Error handiling logic

--- src/Components/FlashcardContainer/FlashcardContainer.js
+++ src/Components/FlashcardContainer/FlashcardContainer.js
@@ -9,7 +9,7 @@ import BrainImage from '../../Images/brain2.png';
 
 export default function FlashcardContainer({ flashcards, loading, categories, selectedCategory, setSelectedCategory, numberOfQuestions, setNumberOfQuestions, handleSubmit }) {
   return (
-    <div className='container'>
+    <div className='container' role="main">
       <form className="header" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="category">Category</label>
@@ -18,22 +18,31 @@ export default function FlashcardContainer({ flashcards, loading, categories, se
               return <option value={category.id} key={category.id}>{category.name}</option>
             })}
           </select>
-
         </div>
         <div className="form-group">
-          <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={numberOfQuestions} onChange={(e) => setNumberOfQuestions(Number(e.target.value))} />
+          <label htmlFor="amount">Number of Questions (Max: 50)</label>
+          <input 
+            type="number" 
+            id="amount" 
+            min="1" 
+            max="50" 
+            step="1" 
+            defaultValue={numberOfQuestions} 
+            onChange={(e) => setNumberOfQuestions(Number(e.target.value))}
+            aria-describedby="amountHint" 
+          />
+          <span id="amountHint" className="visually-hidden">Choose a number between 1 and 50.</span>
         </div>
         <div className="form-group">
-          <button className="btn">Generate</button>
+          <button className="btn" aria-label="Generate flashcards based on selected category and number">Generate</button>
         </div>
       </form>
-      <div className='card-grid'>
+      <div className='card-grid' aria-live="polite"> 
         {flashcards.map(flashcard => {
           return <Flashcard flashcard={flashcard} key={flashcard.id} />;
         })}
-        <Link to="/" className="button go-back-button">Exit The Game</Link>
-        <img src={BrainImage} alt="Brain" />
+        <Link to="/" className="button go-back-button" aria-label="Exit the trivia game and return to home">Exit The Game</Link>
+        <img src={BrainImage} alt="Philosophical representation of a brain and face intertwining with a tree." role="presentation" /> 
       </div>
     </div>
   );
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -17,7 +17,8 @@ const HomePage = () => (
       This app is inspired by one of my countrymen, Avicenna, and his method of switching from one subject to another when he felt tired or stuck. It's a great way to take a break while still engaging your brain!
     </p>
     <Link to="/flashcards" className="goBackButton">Flashcards Game</Link>
-    <a href="https://en.wikipedia.org/wiki/Avicenna" target="_blank" rel="noopener noreferrer" className="button">Learn More About Avicenna</a>
+    <a href="https://en.wikipedia.org/wiki/Avicenna" target="_blank" rel="noopener noreferrer"
+      className="button">Learn More About Avicenna</a>
     <p>
       On a separate note, I used flashcards to entertain my grandson during long-distance video calls in the COVID era. Flashcards can be a versatile and engaging tool for all ages.
       <a href="https://youtu.be/qi4ZWeD4bLk?si=peLVf6tN1-ShiaPy" target="_blank" rel="noopener noreferrer">For a fun video, check out our entertaining flashcard session on YouTube, featuring Pamiri song audio in the background. Be warned – it could be loud!</a>
